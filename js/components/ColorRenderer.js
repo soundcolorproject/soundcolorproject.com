b2952dcb78e92ada81c9c56145a3454022a7b1e7
kgroat <kgroat09@gmail.com>
2020-03-06 16:57:35 -0500

    Making volume & noise have a larger effect

--- js/components/ColorRenderer.js
+++ js/components/ColorRenderer.js
@@ -3,16 +3,14 @@ import { Component } from 'https://unpkg.com/preact?module'
 import { html } from '../html.js'
 import { injectAndObserve } from '../state/injectAndObserve.js'
 import { hsvToHex } from '../color/colorHelpers.js'
+import { dBtoVolume } from '../audio/getAnalysis.js'
 
-const BASE = 1.1
-const MULT = 12
-const levelOld = (value) => Math.pow(BASE, value) * MULT
-const level = (value) => (value) / 100
-const levelNoise = (value) => (value) / 60
+const NOISE_MULT = 1024
+const VIBRANCE_MULT = 4
 function getColorsFromAnalysis(colorMap, { noise, tones }) {
-  const saturationMult = Math.max(0, Math.min(1 - levelNoise(noise), 1))
+  const saturationMult = Math.max(0, Math.min(1 - (dBtoVolume(noise) * NOISE_MULT), 1))
   return tones.map(({ dB, note: { note } }) => {
-    const valueMult = Math.max(0, Math.min(level(dB), 1))
+    const valueMult = Math.max(0, Math.min(dBtoVolume(dB) * VIBRANCE_MULT, 1))
     const { h, s, v } = colorMap[note]
 
     return hsvToHex({
