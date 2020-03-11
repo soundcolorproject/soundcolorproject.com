2b881c5ad927e9b1eb698fcce9f7e955116ba0dc
kgroat <kgroat09@gmail.com>
2020-03-11 11:30:53 -0400

    Fixing low frequency note info

--- js/audio/getNoteInformation.js
+++ js/audio/getNoteInformation.js
@@ -1,10 +1,13 @@
 
 const NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 export function getNoteInformation (frequency) {
-  const noteVal = Math.log2(frequency / 27.5) * 12
+  let noteVal = Math.log2(frequency / 27.5) * 12
+  if (noteVal < 0) {
+    noteVal += 12 * Math.floor((noteVal / -12) + 1)
+  }
   const noteNumber = Math.round(noteVal)
   const cents = (noteVal - noteNumber) * 100
-  const note = NOTES[noteNumber % 12]
+  const note = NOTES[(noteNumber + 12) % 12]
   const octave = Math.floor(noteNumber / 12)
 
   return {
