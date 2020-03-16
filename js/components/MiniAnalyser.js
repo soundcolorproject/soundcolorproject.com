9f64ee0e2476a8d3b5e4de83558cdba108c7da00
kgroat <kgroat09@gmail.com>
2020-03-16 19:09:06 -0400

    Improved mini analyser display

--- js/components/MiniAnalyser.js
+++ js/components/MiniAnalyser.js
@@ -2,12 +2,15 @@
 import { Component } from 'https://unpkg.com/preact?module'
 import { html } from '../html.js'
 import { injectAndObserve } from '../state/injectAndObserve.js'
+import { dBtoVolume } from '../audio/getAnalysis.js'
 
+const BASE = 1.5
 export const MiniAnalyser = injectAndObserve(
   ({ analysis }) => ({ analysis }),
   class MiniAnalyser extends Component {
     render ({ analysis: { miniFft, tones } }) {
-      const heights = [...miniFft].map(dB => Math.max(0, Math.min((100 + dB) / 0.8, 100)))
+      // const heights = [...miniFft].map(dB => Math.max(0, Math.min(100 + dB, 100)))
+      const heights = [...miniFft].map(dB => (BASE ** (dB / 10)) * 100)
       return html`
         <div id="mini-analyser">
           ${
